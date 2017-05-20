45b320517de642b491d9583eb4c303d7b6bdceb7
Srini Raju <srinivasarajui@gmail.com>
2017-05-20 15:25:37 +0530

    basic v1.4

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
 
+console.log("senderId" +senderId);
+console.log("payload" +payload);
   if (payload === "Greeting") {
     // Get user's first name from the User Profile API
     // and include it in the greeting
